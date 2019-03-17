f3728ed2eef7affcd97d6cf09cbd8589efac5af9
mechanizzed <dsibr@hotmail.com>
2019-03-17 14:48:54 -0300

    Added PropTypes in RepositoryCard

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 
+// prop-types
+import PropTypes from "prop-types";
+
 import { Container, RepositoryCard } from "./styles";
 
 const Cards = ({ repositories }) => (
@@ -34,5 +37,22 @@ const Cards = ({ repositories }) => (
     ))}
   </Container>
 );
+
+RepositoryCard.propTypes = {
+  repositories: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number,
+      name: PropTypes.string,
+      owner: PropTypes.shape({
+        login: PropTypes.string,
+        avatar_url: PropTypes.string
+      }),
+      stargazers_count: PropTypes.number,
+      forks_count: PropTypes.number,
+      open_issues_count: PropTypes.number,
+      pushed_at: PropTypes.string
+    })
+  ).isRequired
+};
 
 export default Cards;
